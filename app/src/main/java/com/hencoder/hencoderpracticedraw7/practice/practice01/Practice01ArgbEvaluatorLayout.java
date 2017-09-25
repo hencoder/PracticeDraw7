e0505cf9f31a5905f44fa7abaa74e2e0baaa9c22
lychee <hanliuxin@imtoi.com>
2017-09-25 11:41:13 +0800

    半边旋转了

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice01/Practice01ArgbEvaluatorLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice01/Practice01ArgbEvaluatorLayout.java
@@ -1,9 +1,13 @@
 package com.hencoder.hencoderpracticedraw7.practice.practice01;
 
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.view.animation.AnimationSet;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -29,18 +33,23 @@ public class Practice01ArgbEvaluatorLayout extends RelativeLayout {
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-
         view = (Practice01ArgbEvaluatorView) findViewById(R.id.objectAnimatorView);
         animateBt = (Button) findViewById(R.id.animateBt);
 
         animateBt.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                ObjectAnimator animator = ObjectAnimator.ofInt(view, "color", 0xffff0000, 0xff00ff00);
-                // 在这里使用 ObjectAnimator.setEvaluator() 来设置 ArgbEvaluator，修复闪烁问题
+                ObjectAnimator animator=ObjectAnimator.ofFloat(view,"angle",0f,-30f);
                 animator.setInterpolator(new LinearInterpolator());
-                animator.setDuration(2000);
-                animator.start();
+                animator.setDuration(500);
+
+                ObjectAnimator wave=ObjectAnimator.ofFloat(view,"wave",0f,10f);
+                wave.setInterpolator(new LinearInterpolator());
+                wave.setDuration(500);
+
+                AnimatorSet animationSet=new AnimatorSet();
+                animationSet.playSequentially(animator,wave);
+                animationSet.start();
             }
         });
     }
--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice01/Practice01ArgbEvaluatorView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice01/Practice01ArgbEvaluatorView.java
@@ -1,17 +1,39 @@
 package com.hencoder.hencoderpracticedraw7.practice.practice01;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.hencoder.hencoderpracticedraw7.R;
+
 public class Practice01ArgbEvaluatorView extends View {
 
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Camera camera;
+    private Matrix matrix;
+    private int color = 0xffff0000;
+    private float angle;
+    private float wave;
+    Bitmap bitmap;
+    float[] dst;
+    float[] src;
+    {
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+        angle = 0;
+        wave = 0;
+        camera = new Camera();
+        matrix = new Matrix();
 
-    int color = 0xffff0000;
+                   // 左下
+
+    }
 
     public Practice01ArgbEvaluatorView(Context context) {
         super(context);
@@ -19,6 +41,15 @@ public class Practice01ArgbEvaluatorView extends View {
 
     public Practice01ArgbEvaluatorView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+//        src = {0, 0,                                    // 左上
+//                bitmap.getWidth(), 0,                          // 右上
+//                bitmap.getWidth(), bitmap.getHeight(),        // 右下
+//                0, bitmap.getHeight()};                        // 左下
+//
+//        dst = {0, 0,                                    // 左上
+//                bitmap.getWidth(), 400,                        // 右上
+//                bitmap.getWidth(), bitmap.getHeight() - 200,  // 右下
+//                0, bitmap.getHeight()};
     }
 
     public Practice01ArgbEvaluatorView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -34,14 +65,54 @@ public class Practice01ArgbEvaluatorView extends View {
         invalidate();
     }
 
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+        invalidate();
+    }
+
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        float bitmapWidth = bitmap.getWidth();
+        float bitmapHeight = bitmap.getHeight();
+        float centerX = getWidth() / 2;
+        float centerY = getHeight() / 2;
+        float x = centerX - bitmapWidth / 2;
+        float y = centerY - bitmapHeight / 2;
 
-        int width = getWidth();
-        int height = getHeight();
 
-        paint.setColor(color);
-        canvas.drawCircle(width / 2, height / 2, width / 6, paint);
+        canvas.save();
+        canvas.clipRect(x, y, centerX, (0.5f * bitmapHeight + centerY));
+        canvas.drawBitmap(bitmap, x, y, paint);
+        canvas.restore();
+
+        canvas.save();
+        camera.save();
+        matrix.reset();
+        camera.rotateY(angle);
+        camera.getMatrix(matrix);
+        camera.restore();
+        matrix.postTranslate(centerX, centerY);
+        matrix.preTranslate(-centerX, -centerY);
+        canvas.concat(matrix);
+        canvas.clipRect(centerX, (centerY - 0.5f * bitmapHeight), (centerX + 0.5f * bitmapWidth), (0.5f * bitmapHeight + centerY));
+        canvas.drawBitmap(bitmap, x, y, paint);
+        canvas.restore();
+
+
+    }
+
+
+    public float getWave() {
+        return wave;
+    }
+
+    public void setWave(float wave) {
+        this.wave = wave;
+        invalidate();
     }
 }
